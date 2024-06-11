36aeb69f006eacc1f64757d9b4a6d5f2b0b9c184
sanjaysisodiya11 <131288248+sanjaysisodiya11@users.noreply.github.com>
2024-06-11 14:51:40 +0000

    convertion Operation

---
+++ june_24/01_basic/03_convertionOperation.js
@@ -0,0 +1,10 @@
+let score = null;
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score);
+//console.log(typeof(valueInNumber), valueInNumber);
+
+let isLoginIn = 0;
+let booleanIsLoginIn = Boolean(isLoginIn);
+console.log(booleanIsLoginIn);
