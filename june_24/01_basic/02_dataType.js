046d30ddc2eef6e2cc6cd51669e3e404868cd3f4
sanjaysisodiya11 <131288248+sanjaysisodiya11@users.noreply.github.com>
2024-06-11 14:28:50 +0000

    datatype

---
+++ june_24/01_basic/02_dataType.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//alert(3+3); // we are using node
+
+// Data types
+let name = "sanjay sisodiya" // string datatype
+let age = 31 // integer datatype
+let isLoginIn = false // boolean datatype
+let state; //undefined datatype, right now value not assign
+
+let country = null;//null standalone value
+let users = [
+    "sanjay",
+    "ajay"
+];
+
+console.log(typeof country);
