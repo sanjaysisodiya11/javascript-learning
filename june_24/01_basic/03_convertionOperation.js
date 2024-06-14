71f61d530258d7704a95332a119b4b59d8a69fd7
sanjaysisodiya11 <131288248+sanjaysisodiya11@users.noreply.github.com>
2024-06-14 07:47:59 +0000

    conversition

--- june_24/01_basic/03_convertionOperation.js
+++ june_24/01_basic/03_convertionOperation.js
@@ -1,10 +1,22 @@
-let score = null;
-//console.log(typeof score);
-//console.log(typeof(score));
+let score = undefined;
+/* console.log(typeof score);
+console.log(typeof(score));
 
-//let valueInNumber = Number(score);
-//console.log(typeof(valueInNumber), valueInNumber);
+let valueInNumber = Number(score);
+console.log(valueInNumber); */
 
-let isLoginIn = 0;
-let booleanIsLoginIn = Boolean(isLoginIn);
-console.log(booleanIsLoginIn);
+// 33 = 33 convert into number and type is number
+// "33" = 33 convert into number and type is number
+// "33abdd" = convert into number and type is number but value is "NaN"
+
+let isLogin = null;
+let convertIsLogin = Boolean(isLogin);
+console.log(convertIsLogin);
+
+// "" = true
+// "sanjay" = true
+// 1234566 = true
+// 1 = true
+// null = false
+// undefined = false
+// 54654654sss = 54654654sss is not defined
