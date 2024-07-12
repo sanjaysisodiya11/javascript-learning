39404fe5fc2b56774a2700a2143e7fcae467f211
sanjaysisodiya11 <131288248+sanjaysisodiya11@users.noreply.github.com>
2024-07-12 17:34:54 +0000

    string

--- june_24/01_basic/09_strings.js
+++ june_24/01_basic/09_strings.js
@@ -1,3 +1,19 @@
 const name = "sanjay sisodiya";
 const mobileNo = 9977095377;
-console.log(`My name is ${name} and my mobile number is ${mobileNo}`);
+console.log(`My name is ${name.toUpperCase()} and my mobile number is ${mobileNo}`);
+
+/* ########### STRING DECLARE IN OTHER WAY ######## */
+const gameName = new String('MIRIO-GAME-OLD');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(1));
+console.log(gameName.indexOf('I'));
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(0,2));
+
+const url = "http://www.google.com?sanjay%20singh";
+console.log(url.replace('%20','-'));
+console.log(url.includes('ssssssssss'));
+console.log(gameName.split('-'));
