9991ead9cad249ee320f2dbaf96304b93e3bbf32
sanjaysisodiya11 <131288248+sanjaysisodiya11@users.noreply.github.com>
2024-07-19 16:21:09 +0000

    math and number

--- june_24/01_basic/10_number.js
+++ june_24/01_basic/10_number.js
@@ -1,23 +1,30 @@
-/* const score = 10;
+const score = 100;
+//console.log(score);
 
-const totalNumber = new Number(250);
+const balance = new Number(100);
 
-console.log(score);
-console.log(totalNumber);
-console.log(totalNumber.toFixed(2))
+/* ##### USE NUMBER Method##### */
+const converNumToStr = balance.toString();
+//console.log(balance.toFixed(2));
+//console.log(converNumToStr);
 
-const otherNum = 250.25;
-console.log(otherNum.toPrecision(6));
+const otherNumber = 123.8935336;
+//console.log(otherNumber.toPrecision(6));
 
-const digits = 15688484.25;
-console.log(digits.toLocaleString('en-IN')); */
+const displayNumber = 1000000000;
+//console.log(displayNumber.toLocaleString('en-IN'));
 
-// ###############----MATH-----####################
-console.log(Math.round(1.2)); //1
-console.log(Math.round(1.8)); //2
+/* ################### MATH Library ####################### */
 
-console.log(Math.ceil(1.1)); //2
-console.log(Math.floor(1.9)); //1
-console.log("Square root",Math.sqrt(25)); // 5 i.e 5*5
-console.log(Math.pow(5,3)); //5*5*5
-const arr = [5,9,87,84,12,96];
+/* console.log(Math.abs(-5));
+console.log(Math.round(5.6));
+console.log(Math.ceil(11.2));
+console.log(Math.ceil(11.99));
+console.log(Math.floor(13.99));
+console.log(Math.floor(13.01)); */
+
+//console.log( Math.floor(Math.random()*10)+1 );
+
+const min = 10;
+const max = 50;
+console.log( Math.floor(Math.random()*(max-min + 1) ) + min ) ;
