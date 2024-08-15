5f1d7804cd2ed1ad6e55a965e74edd68cf594d07
sanjaysisodiya11 <131288248+sanjaysisodiya11@users.noreply.github.com>
2024-08-15 05:32:01 +0000

    video25

---
+++ june_24/04_control_flow/one.js
@@ -0,0 +1,28 @@
+/* const score = 200;
+
+if(score > 100 )
+{
+    let power = "fly";
+    console.log(`User power is ${power}`);
+}
+console.log(`User power is ${power}`); */
+
+
+
+/* const balanace = 1000;
+if(balanace>500) console.log("balanace is grether"); */
+
+const month  = 2;
+switch(month) {
+    case 1:
+        console.log("month is Jan");
+        break;
+    case 2:
+        console.log("month is Feb");
+        break;
+    case 3:
+        console.log("month is Mar");
+        break;
+    default:
+        console.log("month na");                 
+}
---
+++ june_24/04_control_flow/truth.js
@@ -0,0 +1,17 @@
+let userEmail = function() {};
+if(userEmail)
+{
+    console.log(`Got user email is: ${userEmail}`);
+}else {
+    console.log(`false value`);
+}
+
+// false
+/* 
+    "" , NaN , null, undefined , 0 , -0
+ */
+
+// truth
+/* 
+"sanjay@tcs.com",  -1 & -2 and so on , {} , [] , function() {}
+ */
