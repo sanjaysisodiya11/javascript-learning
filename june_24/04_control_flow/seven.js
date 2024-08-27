9adc58418671bd95a48117263ce448db09e5a31d
sanjaysisodiya11 <131288248+sanjaysisodiya11@users.noreply.github.com>
2024-08-27 17:50:54 +0000

    DOm

--- june_24/04_control_flow/seven.js
+++ june_24/04_control_flow/seven.js
@@ -24,3 +24,26 @@ const myTotal = totalPrice.reduce( (acc, curVal) => {
     return acc+curVal
 }, 0 )
 console.log(myTotal);
+
+let product = [
+    {
+        title:"phython course",
+        price:700
+    },
+    {
+        title:"Javascript course",
+        price:400
+    },
+    {
+        title:"PHP course",
+        price:200
+    },
+    {
+        title:"Mobile dev course",
+        price:100
+    },
+]
+const cartPrice = product.reduce((acc, item)=> {
+    return acc+item.price
+}, 0)
+console.log(cartPrice);
